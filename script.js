327efda1fc5786257d719102a0a92be819a6b19b
Your Name <you@example.com>
2026-06-01 11:18:45 +0100

    today

---
+++ script.js
@@ -0,0 +1,31 @@
+function showside() {
+	const header2 = document.getElementById ('header2')
+	header2.style.display = 'flex' 
+
+}
+
+function hideside() {
+		const header2 = document.getElementById ('header2')
+		header2.style.display = 'none' 
+}
+
+
+function home() {
+		const header2 = document.getElementById ('header2')
+		header2.style.display = 'none' 
+}
+
+function project() {
+		const header2 = document.getElementById ('header2')
+		header2.style.display = 'none' 
+}
+
+function blog() {
+		const header2 = document.getElementById ('header2')
+		header2.style.display = 'none' 
+}
+
+function contact() {
+		const header2 = document.getElementById ('header2')
+		header2.style.display = 'none' 
+}
