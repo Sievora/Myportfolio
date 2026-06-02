70afcd79909cf7199d161734fe534eb25937c026
Your Name <you@example.com>
2026-06-02 20:15:53 +0100

    last for the day

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const header2 = document.getElementById("header2");
+const nav = document.getElementById("nav");
 
+function showside(){
+    nav.style.right = "20px";
+    nav.style.transition = "0.8s ease";
+
+    nav.style.filter = "blur(0px)";
+}
+
+function hideside(){
+    nav.style.right = "-200px";
+    nav.style.transition = "0.8s ease";
+    
+    nav.style.filter = "blur(10px)";
+}
+
+
+/*gsap.to(nav,  {y: -200, duration: 1})*/
