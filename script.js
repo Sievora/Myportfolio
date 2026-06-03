a22d35cec3c617eabe0d64d599c12726750df468
Your Name <you@example.com>
2026-06-03 11:24:07 +0100

    6/3/2026

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const nav = document.getElementById("nav");
+const whatsapp = document.getElementById("whatsapp");
 
 function showside(){
     nav.style.right = "20px";
     nav.style.transition = "0.8s ease";
-
     nav.style.filter = "blur(0px)";
+    whatsapp.style.display = "none";
 }
 
 function hideside(){
     nav.style.right = "-200px";
     nav.style.transition = "0.8s ease";
-    
     nav.style.filter = "blur(10px)";
+    whatsapp.style.display = "flex";
 }
 
 
